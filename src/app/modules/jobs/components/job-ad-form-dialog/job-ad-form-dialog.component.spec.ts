fd800450e30e3ebbd54901cc0769b6193089b5c4
nikola.arsenovic015 <nikola.arsenovic015@gmail.com>
2024-07-29 21:27:40 +0200

    Fix for eslint issues on spec file

--- src/app/modules/jobs/components/job-ad-form-dialog/job-ad-form-dialog.component.spec.ts
+++ src/app/modules/jobs/components/job-ad-form-dialog/job-ad-form-dialog.component.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
